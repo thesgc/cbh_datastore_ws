7d0a56f4e20435e0d1ad000ceedcaa460dac543b
Strets123 <strets123@gmail.com>
2015-08-18 21:03:40 +0100

    adding the ability to index datapoints

---
+++ cbh_datastore_ws/authorization.py
@@ -0,0 +1,144 @@
+from tastypie.authorization import Authorization
+from tastypie.exceptions import TastypieError, Unauthorized, BadRequest
+import logging
+logger = logging.getLogger(__name__)
+logger_debug = logging.getLogger(__name__)
+from cbh_core_model.models import Project
+from cbh_core_ws.authorization import get_all_project_ids_for_user_perms
+
+
+
+
+
+
+
+
+class DataClassificationProjectAuthorization(Authorization):
+    """
+    Uses permission checking from ``django.contrib.auth`` to map
+    ``POST / PUT / DELETE / PATCH`` to their equivalent Django auth
+    permissions.
+
+    Both the list & detail variants simply check the model they're based
+    on, as that's all the more granular Django's permission setup gets.
+    """
+
+
+    def login_checks(self, request, model_klass, perms=None):
+
+        # If it doesn't look like a model, we can't check permissions.
+        # if not model_klass or not getattr(model_klass, '_meta', None):
+        #     print "improper_setup_of_authorization"
+        #     raise Unauthorized("improper_setup_of_authorization")
+        # User must be logged in to check permissions.
+        if not hasattr(request, 'user'):
+            print "no_logged_in_user"
+            raise Unauthorized("no_logged_in_user")
+
+
+ 
+
+    def read_detail(self, object_list, bundle):
+
+        self.login_checks(bundle.request, bundle.obj.__class__)
+        pids = get_all_project_ids_for_user_perms(bundle.request.user.get_all_permissions(), ["editor","viewer"])
+        allowed = False
+        if bundle.object.l0_permitted_projects.count() == 0:
+            raise BadRequest("You must specify at least one project")
+        for projbundle in bundle.object.l0_permitted_projects.all():
+            #If any one project is not allowed to be edited then unauthorized
+            if projbundle.id in pids:
+                allowed = True
+            else:
+                allowed = False
+                break
+        if allowed:
+            return True
+        raise Unauthorized("not authroized for project")
+
+
+
+
+
+    def create_list(self, object_list, bundle):
+        raise BadRequest("Creating a list is not yet authorized")
+
+
+
+    def update_list(self, object_list, bundle):
+        raise BadRequest("Creating a list is not yet authorized")
+
+
+
+
+
+    def project_ids(self, request, ):
+        pids = get_all_project_ids_for_user_perms( request.user.get_all_permissions(), ["editor","viewer",] )
+        return pids
+
+
+
+
+
+
+
+
+
+
+
+    def create_detail(self, object_list, bundle):
+        self.login_checks(bundle.request, bundle.obj.__class__)
+        pids = get_all_project_ids_for_user_perms(bundle.request.user.get_all_permissions(), ["editor",])
+        allowed = False
+        if len(bundle.data["l0_permitted_projects"]) == 0:
+            raise BadRequest("You must specify at least one project")
+        for projbundle in bundle.data["l0_permitted_projects"]:
+            #If any one project is not allowed to be edited then unauthorized
+            if projbundle.obj.id in pids:
+                allowed = True
+            else:
+                allowed = False
+                break
+        if allowed:
+            return True
+        raise Unauthorized("not authroized for project")
+
+
+
+    def update_detail(self, object_list, bundle):
+        self.login_checks(bundle.request, bundle.obj.__class__)
+        pids = get_all_project_ids_for_user_perms(bundle.request.user.get_all_permissions(), ["editor",])
+        allowed = False
+        if len(bundle.data["l0_permitted_projects"]) == 0:
+            raise BadRequest("You must specify at least one project")
+        for projbundle in bundle.data["l0_permitted_projects"]:
+            #If any one project is not allowed to be edited then unauthorized
+            if projbundle.obj.id in pids:
+                allowed = True
+            else:
+                allowed = False
+                break
+        if allowed:
+            return True
+        raise Unauthorized("not authroized for project")
+
+
+
+
+
+
+
+
+
+    def read_list(self, object_list, bundle):
+        return object_list
+
+
+
+
+
+
+
+
+
+
---
+++ cbh_datastore_ws/elasticsearch_client.py
@@ -0,0 +1,89 @@
+
+from django.conf import settings
+import elasticsearch
+from django.core.exceptions import ImproperlyConfigured
+
+import time
+try:
+    ES_PREFIX = settings.ES_PREFIX
+except AttributeError:
+    raise ImproperlyConfigured("You must set the index prefix for cbh datastore")
+import json
+
+ES_MAIN_INDEX_NAME = "cbh_datastore_index"
+
+def get_index_name():
+    return ES_PREFIX + "__" + ES_MAIN_INDEX_NAME
+
+def delete_main_index():
+    es = elasticsearch.Elasticsearch()
+    try:
+        es.indices.delete(get_index_name())
+    except:
+        pass
+
+def get_client():
+    es = elasticsearch.Elasticsearch()
+    return es
+
+
+
+def index_datapoint_classification(data, index_name=get_index_name()):
+    data = json.loads(data)
+    batches = [data]
+    if data.get("objects", False):
+        batches = data["objects"]
+
+    es = elasticsearch.Elasticsearch()
+    
+    store_type = "niofs"
+    create_body = {
+        "settings": {
+            "index.store.type": store_type
+        },
+        
+         "mappings" : {
+            "_default_" : {
+               "_all" : {"enabled" : False},
+               
+
+               "dynamic_templates" : [ 
+
+                {
+                 "string_fields" : {
+                   "match" : "*",
+                   "match_mapping_type" : "string",
+                   "mapping" : {
+                     "type" : "string","store" : "no", "index_options": "docs","index" : "analyzed", "omit_norms" : True,
+                       "fields" : {
+                         "raw" : {"type": "string","store" : "no", "index" : "not_analyzed", "ignore_above" : 256}
+                       }
+                   }
+                 }
+               } 
+            ]
+        }
+        }
+    }
+    # if(index_name == get_main_index_name()):
+    #     create_body['mappings']['_source'] = { 'enabled':False }
+    #index_name = get_temp_index_name(request, multi_batch_id)
+    
+    es.indices.create(
+            index_name,
+            body=create_body,
+            ignore=400)
+    
+    bulk_items = []
+    for item in batches:
+        bulk_items.append({
+                            "index" :
+                                {
+                                    "_id": str(item["id"]), 
+                                    "_index": index_name,
+                                    "_type": "data_point_classifications"
+                                }
+                            })
+        bulk_items.append(item)
+    #Data is not refreshed!
+    es.bulk(body=bulk_items, refresh=True)
