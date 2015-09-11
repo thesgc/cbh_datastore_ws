57a8394455224e39d8ab08910f62e9c26912a0cd
Strets123 <strets123@gmail.com>
2015-09-11 17:44:16 +0100

    resources tweak

--- cbh_datastore_ws/resources.py
+++ cbh_datastore_ws/resources.py
@@ -241,95 +241,6 @@ autocomplete urls
         '''          '''
         return {"properties" :{bundle.obj.get_space_replaced_name() : self.get_field_values(bundle)[0]}}
 
-    # def dehydrate_filter_form(self, bundle):
-    #     '''          '''
-    #     filter_form = {
-    #                       "htmlClass": "",
-    #                       "key": self.get_namespace_for_action_key(bundle,"filter"),
-    #                       "disableSuccessState": True,
-    #                       "feedback": False,
-    #                       "options": {
-    #                       "refreshDelay": 0,
-    #                         "async": {
-    #                             "url": "tba",
-    #                             "call": "dependencyInjectedBasedOnThisString"
-    #                           }
-    #                       }
-    #                   },
-    #     return {"form" : filter_form }
-
-    # def dehydrate_filter_schema(self, bundle):
-    #     '''          '''
-    #     schema = {
-    #                        self.get_namespace_for_action_key(bundle, "filter"): { 
-    #                           "type": "array", 
-    #                           "format" : "uiselect",
-    #                           "items" :[],
-    #                           "placeholder": "Choose...",
-    #                           "title": "Filter %s" % bundle.obj.name,                                                }
-
-    #                             }
-    #     return {"properties": schema }
-
-
-    # def dehydrate_exclude_form(self, bundle):
-    #     '''          '''
-    #     exclude_form = {
-    #                       "htmlClass": "",
-    #                       "key": self.get_namespace_for_action_key(bundle, "exclude"),
-    #                       "disableSuccessState": True,
-    #                       "feedback": False,
-    #                       "options": {
-    #                       "refreshDelay": 0,
-    #                         "async": {
-    #                             "url": "tba",
-    #                             "call": "dependencyInjectedBasedOnThisString"
-    #                           }
-    #                       }
-    #                   },
-    #     return {"form" : exclude_form }
-
-    # def dehydrate_exclude_schema(self, bundle):
-    #     '''          '''
-    #     schema = {
-    #                        self.get_namespace_for_action_key(bundle, "exclude"): { 
-    #                           "type": "array", 
-    #                           "format" : "uiselect",
-    #                           "items" :[],
-    #                           "placeholder": "Choose...",
-    #                           "title": "Exclude %s" % bundle.obj.name,                                                }
-
-    #                             }
-    #     return {"properties": schema }
-
-
-    # def dehydrate_sort_form(self, bundle):
-    #     hide_form = { 
-    #         "key" : self.get_namespace_for_action_key(bundle, "sort"),
-    #         "type": "radiobuttons",
-    #         "titleMap": [
-    #             { "value": "asc", "name": "A-Z" },
-    #             { "value": "desc", "name": "Z-A" }
-    #         ]
-    #     }
-    #     return {"form" : hide_form}
-
-
-    # def dehydrate_sort_schema(self, bundle):
-    #     '''Note that the sort schema askes for a priority - this is to be used in applying the sorts'''
-    #     hide_schema = {
-    #        self.get_namespace_for_action_key(bundle, "sort"): {
-    #             "type": "string",
-    #             "enum": ["asc","desc"]
-    #           },
-    #           "sort_priority":{
-    #                 "type": "integer"
-    #           }
-    #     }
-    #     return {"properties" : hide_schema}
-
-
-
 
     def dehydrate_actions_form(self, bundle):
         '''          '''
@@ -449,17 +360,12 @@ class DataFormConfigResource(ModelResource):
     name = fields.CharField(null=True,blank=True)
     last_level = fields.CharField(null=True,blank=True)
     l0 = fields.ForeignKey("cbh_datastore_ws.resources.SimpleCustomFieldConfigResource",'l0', null=True, blank=False, readonly=False, help_text=None, full=True)
-    l1 = fields.ForeignKey("cbh_datastore_ws.resources.SimpleCustomFieldConfigResource",'l1', null=True, blank=False, readonly=False, help_text=None,full=True )
-    l2 = fields.ForeignKey("cbh_datastore_ws.resources.SimpleCustomFieldConfigResource",'l2', null=True, blank=False, readonly=False, help_text=None,full=True )
-    l3 = fields.ForeignKey("cbh_datastore_ws.resources.SimpleCustomFieldConfigResource",'l3', null=True, blank=False, readonly=False, help_text=None,full=True )
+    l1 = fields.ForeignKey("cbh_datastore_ws.resources.SimpleCustomFieldConfigResource",'l1', null=True, blank=False, readonly=False, help_text=None,full=True)
+    l2 = fields.ForeignKey("cbh_datastore_ws.resources.SimpleCustomFieldConfigResource",'l2', null=True, blank=False, readonly=False, help_text=None, full=True)
+    l3 = fields.ForeignKey("cbh_datastore_ws.resources.SimpleCustomFieldConfigResource",'l3', null=True, blank=False, readonly=False, help_text=None, full=True)
     l4 = fields.ForeignKey("cbh_datastore_ws.resources.SimpleCustomFieldConfigResource",'l4', null=True, blank=False, readonly=False, help_text=None,full=True)
 
-    l0_key = fields.CharField( null=True, blank=False, readonly=False)
-    l1_key = fields.CharField( null=True, blank=False, readonly=False)
-    l2_key = fields.CharField( null=True, blank=False, readonly=False)
-    l3_key = fields.CharField( null=True, blank=False, readonly=False)
-    l4_key = fields.CharField( null=True, blank=False, readonly=False)
-
+    
     class Meta:
         filtering = {
            "id" : ALL
@@ -558,11 +464,15 @@ The fields that are in this particular custom field config:
         """Returns the last not null custom field config - useful in checking what level the ui should go to"""
         return bundle.obj.last_level()
         
+    def dehydrate(self, bundle):
+        for level in ["l1", "l2", "l3", "l4", "l0"]:
+            levdata = bundle.data.pop(level)
+            if level == bundle.data["last_level"]:
+                bundle.data["main_cfc"] = levdata
+        return bundle
 
 
 
-    def dehydrate_name(self, bundle):
-        bundle.data["name"] = bundle.obj.__unicode__()
 
     def get_schema(self, request, **kwargs):
         """
@@ -579,37 +489,30 @@ The fields that are in this particular custom field config:
         # self.authorized_read_detail(self.get_object_list(bundle.request), bundle)
         return self.create_response(request, self.build_schema())
 
-    def alter_list_data_to_serialize(self, request, data):
-        """Add the level name to each of the fields that need a level name in them"""
-        for level in ["l1", "l2", "l3", "l4", "l0"]:
-            for item in data["objects"]:
-                if item.data[level]:
-                    for field in item.data[level].data["project_data_fields"]:
-                        if field.data["handsontable_column"]["data"]:
-                            field.data["handsontable_column"]["data"] = field.data["handsontable_column"]["data"].format(**{"level": level})
-                        if field.data["elasticsearch_fieldname"]:
-                            field.data["elasticsearch_fieldname"] = field.data["elasticsearch_fieldname"].format(**{"level": level})
-        return data
+    # def alter_list_data_to_serialize(self, request, data):
+    #     """Add the level name to each of the fields that need a level name in them"""
+    #     for level in ["l1", "l2", "l3", "l4", "l0"]:
+    #         for item in data["objects"]:
+    #             if item.data[level]:
+    #                 for field in item.data[level].data["project_data_fields"]:
+    #                     if field.data["handsontable_column"]["data"]:
+    #                         field.data["handsontable_column"]["data"] = field.data["handsontable_column"]["data"].format(**{"level": level})
+    #                     if field.data["elasticsearch_fieldname"]:
+    #                         field.data["elasticsearch_fieldname"] = field.data["elasticsearch_fieldname"].format(**{"level": level})
+    #     return data
 
-    def dehydrate(self, bundle):
-        levels = ["l0", "l1", "l2", "l3", "l4"]
-        for i in range(1,len(levels)+1):
-            level_name = levels[i-1]
-            if bundle.data[level_name]:
-                level_keys = [str(bundle.data[lev].data["id"]) for lev in levels[:i]]
-                
-                bundle.data["%s_key" % level_name] = "_".join(level_keys)
-            else:
-                bundle.data["%s_key" % level_name] = None
-        return bundle
+
+
+    # def dehydrate_natural_key(self, bundle):
+    #     return bundle.obj.natural_key()
+        
 
 
 
 
 class ProjectWithDataFormResource(ModelResource):
     project_type = fields.ForeignKey("cbh_datastore_ws.resources.ProjectTypeResource", 'project_type', blank=False, null=False, full=True)
-    enabled_forms = fields.ToManyField("cbh_datastore_ws.resources.DataFormConfigResource", "enabled_forms", full=True)
-
+    form_uris = fields.DictField(null=True)
     class Meta:
 
         excludes  = ("schemaform", "custom_field_config")
@@ -721,6 +624,29 @@ The fields that are in this particular custom field config:
     autocomplete urls
         '''}
 
+
+    def dehydrate_form_uris(self, bundle):
+        full_dataset = {"enabled_forms" : {}}
+        tree_builder = {}
+        root_object = None
+        for dfc in bundle.obj.enabled_forms.all():
+            root_object = dfc.get_all_ancestor_objects(bundle.request, tree_builder=tree_builder)
+        for key, obj_list in tree_builder.iteritems():
+            for i, obj in enumerate(obj_list):
+                dfcres = DataFormConfigResource()
+                bun = dfcres.build_bundle(obj=obj, request=bundle.request)
+                bun = dfcres.full_dehydrate(bun)
+                tree_builder[key][i] = bun.data["id"]
+                full_dataset["enabled_forms"][bun.data["id"]] = bun.data
+
+        root_obj = tree_builder.pop("root")
+        full_dataset["root_data_form_config_id"] = root_obj[0]
+        full_dataset["permitted_routes_tree"] = tree_builder
+
+        return full_dataset
+
+
+
     def get_schema(self, request, **kwargs):
         """
         Returns a serialized form of the schema of the resource.
@@ -800,8 +726,7 @@ It has the following fields:
 
 
 class MyForeignKey(fields.ForeignKey):
-    def should_full_dehydrate(self, bundle, for_list):
-        
+    def should_full_dehydrate(self, bundle, for_list):     
         return bundle.request.GET.get("full", None)
 
 
@@ -1389,6 +1314,8 @@ class NestedDataPointClassificationResource(DataPointClassificationResource):
             "l4" : ALL_WITH_RELATIONS,
             "parent" : ALL_WITH_RELATIONS
         }
+
+
 
 
 
