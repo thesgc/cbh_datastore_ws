a0af38040dc9352d251b527706e491530593529b
strets123 <strets123@gmail.com>
2016-01-17 20:39:57 +0000

    Update datastore_realdata.py

--- cbh_datastore_ws/features/steps/datastore_realdata.py
+++ cbh_datastore_ws/features/steps/datastore_realdata.py
@@ -40,7 +40,9 @@ def create_realdata(context):
     context.test_case.assertEqual(context.dfc.l4_id, None)
 
 
-@given("I create a project and add the data")
+
+
+@given("I create a project and add the forms")
 def project(context):
     from cbh_core_model.models import Project, CustomFieldConfig, PinnedCustomField, ProjectType, DataType, DataFormConfig
     from cbh_datastore_model.models import DataPoint, DataPointClassification, DataPointClassificationPermission
@@ -86,60 +88,3 @@ def project(context):
     tree_builder = {}
     context.dfc.get_all_ancestor_objects(context,tree_builder=tree_builder)
 
-    l0_data = parser.get_sheet("src/cbh_datastore_ws/cbh_datastore_ws/features/fixtures/sample_data.xlsx", "Project")
-    l0_datapoint, created = DataPoint.objects.get_or_create(
-        custom_field_config=context.dfc.l0, project_data=l0_data[0][0], created_by=context.user, )
-    context.l0_dpc = DataPointClassification.objects.create(
-         l0_id=l0_datapoint.id, data_form_config_id=4, created_by=context.user)
-    DataPointClassificationPermission.objects.create(
-        project=project, data_point_classification=context.l0_dpc)
-
-    first_l1_data = parser.get_sheet(
-        "src/cbh_datastore_ws/cbh_datastore_ws/features/fixtures/sample_data.xlsx", "Sub-project")
-    first_l1_datapoint = DataPoint.objects.create(
-        custom_field_config=context.dfc.l1, project_data=first_l1_data[0][0], created_by=context.user)
-    context.first_l1_dpc = DataPointClassification.objects.create(
-        parent=context.l0_dpc, l0_id=l0_datapoint.id, l1_id=first_l1_datapoint.id, data_form_config_id=3, created_by=context.user)
-    DataPointClassificationPermission.objects.create(
-        project=project, data_point_classification=context.first_l1_dpc)
-
-    second_l1_data = parser.get_sheet(
-        "src/cbh_datastore_ws/cbh_datastore_ws/features/fixtures/sample_data.xlsx", "Sub-project_2")
-    second_l1_datapoint = DataPoint.objects.create(
-        custom_field_config=context.dfc.l1, project_data=first_l1_data[0][0], created_by=context.user)
-    context.second_l1_dpc = DataPointClassification.objects.create(
-        parent=context.l0_dpc, l0_id=l0_datapoint.id, l1_id=second_l1_datapoint.id, data_form_config_id=3, created_by=context.user)
-    DataPointClassificationPermission.objects.create(
-        project=project, data_point_classification=context.second_l1_dpc)
-
-    for assay, parent in (
-        ("Alpha Screen", context.first_l1_dpc),
-        ("Thermal Shift", context.first_l1_dpc),
-        ("Cell Viab", context.second_l1_dpc),
-        ("Cell Viab 2", context.second_l1_dpc),
-        ("Luciferase", context.second_l1_dpc)
-    ):
-        assay_def = parser.get_sheet(
-            "src/cbh_datastore_ws/cbh_datastore_ws/features/fixtures/sample_data.xlsx", "%s assay definition" % assay)
-        assay_def_dp = DataPoint.objects.create(
-            custom_field_config=context.dfc.l2, project_data=assay_def[0][0], created_by=context.user)
-        assay_def_dpc = DataPointClassification.objects.create(
-            parent=parent, l0_id=l0_datapoint.id, l2_id=assay_def_dp.id, l1_id=first_l1_datapoint.id, data_form_config_id=2, created_by=context.user)
-        DataPointClassificationPermission.objects.create(
-            project=project, data_point_classification=assay_def_dpc)
-
-        for point in parser.get_sheet("src/cbh_datastore_ws/cbh_datastore_ws/features/fixtures/sample_data.xlsx", "%s raw data" % assay)[0]:
-            activity_dp = DataPoint.objects.create(
-                custom_field_config=context.dfc.l3,
-                project_data=point,
-                created_by=context.user)
-            context.activity_dpc = DataPointClassification.objects.create(
-                parent=assay_def_dpc,
-                l0_id=l0_datapoint.id,
-                l1_id=first_l1_datapoint.id,
-                l2_id=assay_def_dp.id,
-                l3_id=activity_dp.id,
-                data_form_config=context.dfc,
-                created_by=context.user)
-            DataPointClassificationPermission.objects.create(
-                project=project, data_point_classification=context.activity_dpc)
